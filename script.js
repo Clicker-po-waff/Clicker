aea7e7df79189fa085233336cf351d689f68cece
Clicker-po-waff <sanatnurasyl90@gmail.com>
2024-06-26 03:11:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -10,12 +10,14 @@ function handleClick() {
 
     // Запуск анимации GIF
     let waffImage = document.getElementById('waff');
-    waffImage.src = ""; // Сброс GIF для перезапуска анимации
+    waffImage.src = "images/waff.gif";
+    
+    // Вернуть статичное изображение после завершения анимации (например, через 2 секунды)
     setTimeout(() => {
-        waffImage.src = "images/waff.gif";
-    }, 10);
+        waffImage.src = "images/waff_static.png";
+    }, 1000); // Настройте время в соответствии с длительностью вашего GIF
 
     if (counter === 1000000) {
-        alert('Тебе было скучно и ты погладил вафф 1 миллион раз? Уф сигма');
+        alert('Тебе было скучно и ты погладил вафф 1 миллион раз? Уф сигма'');
     }
 }
