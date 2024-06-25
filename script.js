3ea70718a4452e147e59e76aeb036b0ae7a06664
Clicker-po-waff <sanatnurasyl90@gmail.com>
2024-06-26 04:00:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -32,5 +32,5 @@ function animateSprite() {
             waffImage.src = `sprites/waff${currentFrame + 1}.png`;
             currentFrame++;
         }
-    }, 5); // Настройте скорость анимации
+    }, 99999); // Настройте скорость анимации
 }
